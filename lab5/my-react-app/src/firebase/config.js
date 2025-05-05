493baaf09af4924b7f3fa8f826eb50cd8a289429
kvit-dev <kvitkadkv@gmail.com>
2025-05-05 23:53:02 +0300

    config commit

---
+++ lab5/my-react-app/src/firebase/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDiXPVVDwqh258kIbNl9J7clXjmL3_bmF0",
+  authDomain: "lab4-web-3d2ee.firebaseapp.com",
+  projectId: "lab4-web-3d2ee",
+  storageBucket: "lab4-web-3d2ee.firebasestorage.app",
+  messagingSenderId: "237052738362",
+  appId: "1:237052738362:web:7f6ae2a308506db69cee03"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
