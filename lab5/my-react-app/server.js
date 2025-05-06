bd729ed0ce6d83046e9f02ac2ca933c268b569e0
kvit-dev <kvitkadkv@gmail.com>
2025-05-06 23:53:12 +0300

    error fixing

--- lab5/my-react-app/server.js
+++ lab5/my-react-app/server.js
@@ -179,7 +179,7 @@ app.get("/api/user/ratings", verifyToken, async (req, res) => {
   }
 });
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
