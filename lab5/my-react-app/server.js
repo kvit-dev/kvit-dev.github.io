54e9b16f43f6ddf15603831b4e03064f8ca9b092
kvit-dev <kvitkadkv@gmail.com>
2025-05-07 19:56:06 +0300

    server.js fix

--- lab5/my-react-app/server.js
+++ lab5/my-react-app/server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-/*app.use(express.static(path.join(__dirname, "build")));*/
+app.use(express.static(path.join(__dirname, "build")));
 
 const verifyToken = async (req, res, next) => {
   const token = req.headers.authorization?.split("Bearer ")[1];
@@ -179,9 +179,9 @@ app.get("/api/user/ratings", verifyToken, async (req, res) => {
   }
 });
 
-/*app.get("/*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
-});*/
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
